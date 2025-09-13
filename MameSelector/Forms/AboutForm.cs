2388417ca5264fe73e0475793ddd1f35cc5794a1
Charles D'Aoust <charles.daoust@gmail.com>
2025-09-13 19:03:17 -0400

    Remove fallback version strings - use csproj version exclusively

--- MameSelector/Forms/AboutForm.cs
+++ MameSelector/Forms/AboutForm.cs
@@ -16,17 +16,9 @@ namespace MameSelector.Forms
 
         private void SetVersion()
         {
-            try
-            {
-                var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
-                var versionString = version != null ? $"{version.Major}.{version.Minor}.{version.Build}" : "0.4.4";
-                versionLabel.Text = $"Version {versionString}";
-            }
-            catch
-            {
-                // Fallback version if assembly version cannot be read
-                versionLabel.Text = "Version 0.4.4";
-            }
+            var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            var versionString = $"{version.Major}.{version.Minor}.{version.Build}";
+            versionLabel.Text = $"Version {versionString}";
         }
 
         private void LoadIcon()
